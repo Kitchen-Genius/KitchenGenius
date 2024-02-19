6f85d64f6284b215e9aff57067f336f26c199621
Reshaflame“ <reshaflame@gmail.com>
2024-02-19 18:57:48 +0200

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 # main.py
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.ingredients_routes import router as ingredients_router  # Adjust import path as necessary
+from app.services.spoonacular import fetch_recipe
+from app.database.db import client
 from dotenv import load_dotenv
 import os
+
+
 
 load_dotenv()
 SPOONACULAR_API_KEY = os.getenv("SPOONACULAR_API_KEY")
 
 app = FastAPI()
 
-from app.services.spoonacular import fetch_recipe
-from app.database.db import client
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(ingredients_router)
 
