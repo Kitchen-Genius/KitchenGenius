77234ac21f87ebac82b5b698707e2c2f4d3aa4b4
Reshaflame“ <reshaflame@gmail.com>
2024-02-21 15:48:30 +0200

    recipes

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 # main.py
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes.ingredients_routes import router as ingredients_router  # Adjust import path as necessary
-from app.services.spoonacular import fetch_recipe
-from app.database.db import client
+# from app.routes.ingredients_routes import router as ingredients_router  # Adjust import path as necessary
+from app.services.spoonacular import search_recipes
+# from app.database.db import client
 from dotenv import load_dotenv
 import os
 
@@ -22,22 +22,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(ingredients_router)
+# app.include_router(ingredients_router)
 
-@app.on_event("startup")
-async def startup_event():
+# @app.on_event("startup")
+# async def startup_event():
     # Example: Connect to the database (if needed)
-    pass
+#     pass
 
-@app.on_event("shutdown")
-async def shutdown_event():
+# @app.on_event("shutdown")
+# async def shutdown_event():
     # Example: Close database connection (if needed)
-    await client.close()
+#     await client.close()
 
 @app.get("/recipes/")
-async def get_recipes(ingredients: str):
+async def get_recipes(
+    diet: str = None,
+    includeIngredients: str = None,
+    type: str = None,
+    intolerances: str = None,
+    instructionsRequired: bool = True
+):
     try:
-        recipes = await fetch_recipe(ingredients)
+        recipes = search_recipes(diet=diet, includeIngredients=includeIngredients, type=type, intolerances=intolerances, instructionsRequired=instructionsRequired)
         return recipes
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
