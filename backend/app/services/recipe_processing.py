e4544da9fb31724cc0bdc1519a84ea1e6fe421cd
Reshaflame“ <reshaflame@gmail.com>
2024-02-22 18:56:23 +0200

    recipe process update commit

--- backend/app/services/recipe_processing.py
+++ backend/app/services/recipe_processing.py
@@ -4,6 +4,11 @@ def process_recipe(recipe_json, ingredients_json):
     recipe = recipe_json['results'][0] if recipe_json['results'] else {}
     
     processed_recipe = {
+        "vegetarian": recipe.get("vegetarian"),
+        "vegan": recipe.get("vegan"),
+        "glutenFree": recipe.get("glutenFree"),
+        "dairyFree": recipe.get("dairyFree"),
+        "veryHealthy": recipe.get("veryHealthy"),
         "id": recipe.get("id"),
         "title": recipe.get("title"),
         "image": recipe.get("image"),
