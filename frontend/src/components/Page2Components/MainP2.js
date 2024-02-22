09d13a16202f1cc3b9d284a2965628ff644298d7
regevshema <regevyoel@gmail.com>
2024-02-22 21:16:48 +0200

    Untrack node_modules directory

--- frontend/src/components/Page2Components/MainP2.js
+++ frontend/src/components/Page2Components/MainP2.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import Json from '../../csv_files/10recipes.json';
+import Json from '../../csv_files/main course_processed_recipes.json';
+import axios from 'axios'; 
 
 export default function MainP2(props) {
   const [selector, setSelector] = useState([]);
@@ -14,64 +15,70 @@ export default function MainP2(props) {
   };
 
   useEffect(() => {
-    setRecipeJson(Json);
+    axios.get('../../csv_files/10recipes.json')
+    .then((response) => {
+      // Handle successful response
+      setRecipeJson(response.data);
+    })
+    .catch((error) => {
+      // Handle error
+      console.error('Error fetching JSON:', error);
+    });
     console.log(recipeJson);
 
     const putRecipeCard = () => {
       Object.entries(recipeJson).forEach(([key, value]) => {
         let shouldRender = true;
+        let countIngrediets = 0;
+        Object.entries(value.ingredients).forEach(([key2, value2]) => { 
+              Object.entries(props.ingredientList).forEach(([key3, value3]) => { 
+                  if(value3.name === value2.name) {
 
-        Object.entries(value).forEach(([key2, value2]) => { 
- 
+                      countIngrediets += 1;
+                      console.log(countIngrediets);
 
-          if (props.ingredientList.hasOwnProperty('Vegetarian') && value2.vegetarian === false) { 
+                  }
+
+
+              });
+        });
+
+
+          if (props.ingredientList.hasOwnProperty('Vegetarian') && value.vegetarian === false) { 
             shouldRender = false;
           }
 
-          if (props.ingredientList.hasOwnProperty('veryHealthy') && value2.veryHealthy === false) {
+          if (props.ingredientList.hasOwnProperty('veryHealthy') && value.veryHealthy === false) {
             shouldRender = false;
           }
 
-          if (props.ingredientList.hasOwnProperty('dairyFree') && value2.dairyFree === false) {
+          if (props.ingredientList.hasOwnProperty('dairyFree') && value.dairyFree === false) {
             shouldRender = false;
           }
-
+          if (countIngrediets > 1){
           if (shouldRender) {
             setSelector((prevSelector) => [
               ...prevSelector,
               <>
                 <div className='leftContainer'>
-                  <h1 className='subTitle'>{value2.title}</h1>
+                  <h1 className='subTitle'>{value.title}</h1>
                   <div className= 'text_container'>
                     <p className='time'>time: {getRandomTime()}</p>
                     <p className='difficulty'></p>
-                    <p className='reviews'>rating: {value2.spoonacularScore.toFixed(1)}</p>
+                    
                     <button className='start_button'>Start</button>
                   </div>
                 </div>
                 <div className='rightContainer'>
-                  <div className='recPic'><img src={value2.image} alt="img" /></div>
+                  <div className='recPic'><img src={value.image} alt="img" /></div>
                 </div>
               </>
             ]);
           }
-        });
-
-        if (key === 'id' && !alreadyDisplayed.includes(value)) {
-          setAlreadyDisplayed((prevDisplayed) => [...prevDisplayed, value]);
-          console.log(alreadyDisplayed);
-
-          let title = '';
-          let spoonacularScore = '';
-
-          Object.entries(recipeJson).forEach(([key, value]) => {
-            if (key === 'title') {
-              title = value;
-            } else if (key === 'spoonacularScore') {
-              spoonacularScore = value;
-            }
-          });
         }
+        
+
+
       });
     };
 
